704423b45c97f958dbba37e4a10ae3a58becb38a
Adam Evans <addauk@gmail.com>
2023-03-20 15:13:55 +0000

    Added deposit function and happy path tests
    Deposit makes use of a private function to format date for the day.
    Tests intercept Date.now() to force all testing dates to the same day, enabling testing.

--- __tests__/bank.spec.js
+++ __tests__/bank.spec.js
@@ -3,6 +3,18 @@
 const { beforeEach } = require("node:test");
 const Bank = require("../bank");
 
+let dateSpy;
+
+beforeAll(() => {
+  dateSpy = jest
+    .spyOn(global.Date, "now")
+    .mockImplementation(() => new Date("January 1,1999"));
+});
+
+afterAll(() => {
+  dateSpy.mockRestore();
+});
+
 describe("Bank class", () => {
   it("constructs", () => {
     let bank = new Bank();
@@ -10,12 +22,22 @@ describe("Bank class", () => {
     expect(bank.balance).toBe(0);
   });
   describe("deposit method", () => {
-    it.todo(
-      "stores the value given alongside today's date and correctly updates balance"
-    );
-    it.todo(
-      "stores multiple deposits alongside correct dates and correctly updates balance"
-    );
+    it("stores the value given alongside today's date and correctly updates balance", () => {
+      let bank = new Bank();
+      bank.deposit(100);
+      expect(bank.transactions).toEqual([{ date: "01/01/1999", amount: 100 }]);
+      expect(bank.balance).toBe(100);
+    });
+    it("stores multiple deposits alongside correct dates and correctly updates balance", () => {
+      let bank = new Bank();
+      bank.deposit(100);
+      bank.deposit(1514.27);
+      expect(bank.transactions).toEqual([
+        { date: "01/01/1999", amount: 100 },
+        { date: "01/01/1999", amount: 1514.27 },
+      ]);
+      expect(bank.balance).toBe(1614.27);
+    });
   });
   describe("withdraw method", () => {
     it.todo(
--- bank.js
+++ bank.js
@@ -5,6 +5,24 @@ class Bank {
     this.balance = 0.0;
     this.transactions = [];
   }
+
+  deposit(amount) {
+    this.balance += amount;
+
+    this.transactions.push({ date: this.#todayFormatted(), amount: amount });
+  }
+
+  #todayFormatted() {
+    let today = new Date(Date.now());
+    let yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1;
+    let dd = today.getDate();
+    dd = dd.toString().padStart(2, 0);
+    mm = mm.toString().padStart(2, 0);
+    let formattedToday = dd + "/" + mm + "/" + yyyy;
+
+    return formattedToday;
+  }
 }
 
 module.exports = Bank;
