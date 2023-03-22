914262fea2070e6a4d8778f9b94e458053b35f4a
Adam Evans <addauk@gmail.com>
2023-03-22 09:30:41 +0000

    Completed tests for StatementPrinter
    Mocked transaction values

--- __tests__/statementPrinter.spec.js
+++ __tests__/statementPrinter.spec.js
@@ -17,31 +17,29 @@ describe("StatementPrinter Class", () => {
     );
   });
 
-  // it("prints a statement that shows deposits and withdrawls in order with correctly tracking balances", () => {
-  //   // let bank = new Bank();
-  //   // dateSpy.mockImplementationOnce(() => new Date("January 10,2023"));
-  //   // bank.deposit(1000);
-  //   // dateSpy.mockImplementationOnce(() => new Date("January 13,2023"));
-  //   // bank.deposit(2000);
-  //   // dateSpy.mockImplementationOnce(() => new Date("January 14,2023"));
-  //   // bank.withdraw(500);
-  //   //need mocking
-  //   StatementPrinter.print(transactions, balance);
-  //   expect(consoleSpy).toHaveBeenNthCalledWith(
-  //     1,
-  //     "date || credit || debit || balance"
-  //   );
-  //   expect(consoleSpy).toHaveBeenNthCalledWith(
-  //     2,
-  //     "14/01/2023 || || 500.00 || 2500.00"
-  //   );
-  //   expect(consoleSpy).toHaveBeenNthCalledWith(
-  //     3,
-  //     "13/01/2023 || 2000.00 || || 3000.00"
-  //   );
-  //   expect(consoleSpy).toHaveBeenNthCalledWith(
-  //     4,
-  //     "10/01/2023 || 1000.00 || || 1000.00"
-  //   );
-  // });
+  it("prints a statement that shows deposits and withdrawls in order with correctly tracking balances", () => {
+    let balance = 2500.0;
+    let transactions = [
+      { date: "10/01/2023", amount: 1000.0 },
+      { date: "13/01/2023", amount: 2000.0 },
+      { date: "14/01/2023", amount: -500.0 },
+    ];
+    StatementPrinter.print(transactions, balance);
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      1,
+      "date || credit || debit || balance"
+    );
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      2,
+      "14/01/2023 || || 500.00 || 2500.00"
+    );
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      3,
+      "13/01/2023 || 2000.00 || || 3000.00"
+    );
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      4,
+      "10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
 });
